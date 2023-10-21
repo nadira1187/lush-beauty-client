1c4ec63cd3c696981f179d64bf2dc984f7afaae9
nadira1187 <nadira35-1187@s.diu.edu.bd>
2023-10-21 17:45:12 +0600

    added slider image on brand details page

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -26,7 +26,7 @@ const ProductList = () => {
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                    <img src="https://i.ibb.co/G5ZBj60/s4.jpg" className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
